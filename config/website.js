70257328bbf0db66d8d9c4b2bebf649817b83b40
victorxdev <33047917+victorxdev@users.noreply.github.com>
2019-03-18 21:06:47 +0800

    Update website.js

--- config/website.js
+++ config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Victor Tan | Digital Portfolio' // Navigation and Site Title
-  siteTitleAlt: 'Victor', // Alternative Site title for SEO
-  siteTitleShort: 'Victor', // short_name for manifest
-  siteHeadline: 'Victor Tan | Digital Portfolio', // Headline for schema.org JSONLD
-  siteUrl: 'https://victortqw.netlify.com/', // Domain of your site. No trailing slash!
+  siteTitle: 'Victor - Digital Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Victor Digital Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Victor Digital Portfolio', // short_name for manifest
+  siteHeadline: 'Victor - Digital Portfolio', // Headline for schema.org JSONLD
+  siteUrl: 'https://victortqw.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Victor Professional Portfolio',
-  author: 'Victor Tan Qin Wei', // Author for schema.org JSONLD
+  siteDescription: 'Victor Digital Portfolio',
+  author: 'Victor Digital Portfolio', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@victorxmusic', // Twitter Username
   ogSiteName: 'victortqw', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-',
+  googleAnalyticsID: 'UA',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
