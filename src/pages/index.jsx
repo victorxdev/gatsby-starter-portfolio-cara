48a8c71ac5466b149b82cfeff5bcfefa8974b74e
victorxdev <33047917+victorxdev@users.noreply.github.com>
2019-03-18 22:14:44 +0800

    new

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,115 +20,102 @@ import Contact from '../views/Contact'
 import avatar from '../images/avatar.jpg'
 
 const ProjectsWrapper = styled.div`
-  ${tw`flex flex-wrap justify-between mt-8`};
-  display: grid;
-  grid-gap: 4rem;
-  grid-template-columns: repeat(2, 1fr);
-  @media (max-width: 1200px) {
-    grid-gap: 3rem;
-  }
-  @media (max-width: 900px) {
-    grid-template-columns: 1fr;
-    grid-gap: 2rem;
-  }
+${tw`flex flex-wrap justify-between mt-8`};
+display: grid;
+grid-gap: 4rem;
+grid-template-columns: repeat(2, 1fr);
+@media (max-width: 1200px) {
+grid-gap: 3rem;
+}
+@media (max-width: 900px) {
+grid-template-columns: 1fr;
+grid-gap: 2rem;
+}
 `
 
 const AboutHero = styled.div`
-  ${tw`flex flex-col lg:flex-row items-center mt-8`};
+${tw`flex flex-col lg:flex-row items-center mt-8`};
 `
 
 const Avatar = styled.img`
-  ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
+${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
 `
 
 const AboutSub = styled.span`
-  ${tw`text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl`};
+${tw`text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl`};
 `
 
 const AboutDesc = styled.p`
-  ${tw`text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify`};
+${tw`text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify`};
 `
 
 const ContactText = styled.p`
-  ${tw`text-grey-light font-sans text-xl md:text-2xl lg:text-3xl`};
+${tw`text-grey-light font-sans text-xl md:text-2xl lg:text-3xl`};
 `
 
-const Footer = styled.footer`
-  ${tw`text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
-`
 
 const Index = () => (
-  <>
-    <Layout />
-    <Parallax pages={5}>
-      <Hero offset={0}>
-        <BigTitle>
-          Hello, <br /> I'm Victor.
-        </BigTitle>
-        <Subtitle>Welcome to my online portfolio!</Subtitle>
-      </Hero>
-      <Projects offset={1}>
-        <Title>Projects</Title>
-        <ProjectsWrapper>
-          <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
-            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
-          >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
-          </ProjectCard>
-          <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
-          >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
-          </ProjectCard>
-          <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
-          >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
-          </ProjectCard>
-          <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
-            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
-          >
-            A fantasy image manipulation relocating the habitat of wild animals.
-          </ProjectCard>
-        </ProjectsWrapper>
-      </Projects>
-      <About offset={3}>
-        <Title>About</Title>
-        <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
-          <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
-          </AboutSub>
-        </AboutHero>
-        <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
-        </AboutDesc>
-      </About>
-      <Contact offset={4}>
-        <Inner>
-          <Title>Get in touch</Title>
-          <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
-          </ContactText>
-        </Inner>
-      </Contact>
-    </Parallax>
-  </>
+<>
+<Layout />
+<Parallax pages={5}>
+<Hero offset={0}>
+<BigTitle>
+Hello, <br /> I'm Victor.
+</BigTitle>
+<Subtitle>Welcome to my online portfolio!</Subtitle>
+</Hero>
+<Projects offset={1}>
+<Title>Experience</Title>
+<ProjectsWrapper>
+<ProjectCard
+title="Digital Marketing Executive | Funding Societies"
+link="https://fundingsocieties.com/"
+bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+>
+Funding Societies | Modalku is the largest Peer-to-Peer lending platform in Southeast Asia. 
+It provides business financing to SMEs which is crowdfunded by retail and institutional investors. 
+</ProjectCard>
+<ProjectCard
+title="Marketing Lead | CareerSocius"
+link="https://www.careersocius.com/"
+bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+>
+CareerSocius is a social enterprise that helps job-seekers in Singapore maximize their career potential. 
+With a diverse team of Branding Buddies spanning across various industries, we offer the relevant expertise and experience to guide job seekers in their job search.
+</ProjectCard>
+</ProjectsWrapper>
+</Projects>
+<About offset={2}>
+<Title>About</Title>
+<AboutHero>
+<Avatar src={avatar} alt="Victor Tan" />
+<AboutSub>
+Victor Tan | Digital Marketer & Fintech Enthusiast
+</AboutSub>
+</AboutHero>
+<AboutDesc>
+Adaptable, motivated and self-driven Economics & Finance major with cross-disciplinary skills sets involving marketing, analytics, finance, design and strategy. I am passionate about the FinTech industry and the use of technology to improve and automate financial activities. Being a social geek, I love to keep up and discuss latest technologies & digital trends. 
+
+In my free time, I enjoy playing the guitar, board/video gaming as well as hiking, kayaking & exploring the outdoors.
+
+I'm fascinated about personality assessments & quizzes. Here's a list of my personality profiles:
+Ideation-Strategic-Restorative-Communication-Command (StrengthsFinder2.0) 
+ENTP (MBTI) 
+Loyal-Skeptic (Enneagram) 
+Mid-O High-C High-E Mid-A Low-N (Big5)
+Fire (TetraMap)
+</AboutDesc>
+</About>
+<Contact offset={3}>
+<Inner>
+<Title>Contact</Title>
+<ContactText>
+            Say <a href="mailto:victor.tqw@live.com">Hi</a> or follow me on <a href="https://www.instagram.com/victorxmusic/">Instagram</a> & <a href="https://www.linkedin.com/in/victortqw/">LinkedIn</a> 
+</ContactText>
+</Inner>
+</Contact>
+</Parallax>
+</>
 )
 
 export default Index
