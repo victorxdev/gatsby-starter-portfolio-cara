f1bf7a4e24108a55c1208c1370a1266b52215ded
victorxdev <33047917+victorxdev@users.noreply.github.com>
2019-03-22 22:29:15 +0800

    v1.1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'tailwind.macro'
-import { Parallax } from 'react-spring/renderprops-addons.cjs'
+import React from "react";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+import { Parallax } from "react-spring/renderprops-addons.cjs";
 
 // Components
-import Layout from '../components/Layout'
-import ProjectCard from '../components/ProjectCard'
+import Layout from "../components/Layout";
+import ProjectCard from "../components/ProjectCard";
 
 // Elements
-import Inner from '../elements/Inner'
-import { Title, BigTitle, Subtitle } from '../elements/Titles'
+import Inner from "../elements/Inner";
+import { Title, BigTitle, Subtitle } from "../elements/Titles";
 
 // Views
-import Hero from '../views/Hero'
-import Projects from '../views/Projects'
-import About from '../views/About'
-import Contact from '../views/Contact'
+import Hero from "../views/Hero";
+import Projects from "../views/Projects";
+import About from "../views/About";
+import Contact from "../views/Contact";
 
-import avatar from '../images/avatar.jpg'
+import avatar from "../images/avatar.jpg";
 
 const ProjectsWrapper = styled.div`
 ${tw`flex flex-wrap justify-between mt-8`};
@@ -31,91 +31,101 @@ grid-gap: 3rem;
 grid-template-columns: 1fr;
 grid-gap: 2rem;
 }
-`
+`;
 
 const AboutHero = styled.div`
 ${tw`flex flex-col lg:flex-row items-center mt-8`};
-`
+`;
 
 const Avatar = styled.img`
 ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
-`
+`;
 
 const AboutSub = styled.span`
 ${tw`text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl`};
-`
+`;
 
 const AboutDesc = styled.p`
 ${tw`text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify`};
-`
+`;
 
 const ContactText = styled.p`
 ${tw`text-grey-light font-sans text-xl md:text-2xl lg:text-3xl`};
-`
+`;
 
+const Footer = styled.footer`
+${tw`text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
+`;
 
 const Index = () => (
-<>
-<Layout />
-<Parallax pages={5}>
-<Hero offset={0}>
-<BigTitle>
-Hello, <br /> I'm Victor.
-</BigTitle>
-<Subtitle>Welcome to my online portfolio!</Subtitle>
-</Hero>
-<Projects offset={1}>
-<Title>Experience</Title>
-<ProjectsWrapper>
-<ProjectCard
-title="Digital Marketing Executive | Funding Societies"
-link="https://fundingsocieties.com/"
-bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
->
-Funding Societies | Modalku is the largest Peer-to-Peer lending platform in Southeast Asia. 
-It provides business financing to SMEs which is crowdfunded by retail and institutional investors. 
-</ProjectCard>
-<ProjectCard
-title="Marketing Lead | CareerSocius"
-link="https://www.careersocius.com/"
-bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
->
-CareerSocius is a social enterprise that helps job-seekers in Singapore maximize their career potential. 
-With a diverse team of Branding Buddies spanning across various industries, we offer the relevant expertise and experience to guide job seekers in their job search.
-</ProjectCard>
-</ProjectsWrapper>
-</Projects>
-<About offset={2}>
-<Title>About</Title>
-<AboutHero>
-<Avatar src={avatar} alt="Victor Tan" />
-<AboutSub>
-Victor Tan | Digital Marketer & Fintech Enthusiast
-</AboutSub>
-</AboutHero>
-<AboutDesc>
-Adaptable, motivated and self-driven Economics & Finance major with cross-disciplinary skills sets involving marketing, analytics, finance, design and strategy. I am passionate about the FinTech industry and the use of technology to improve and automate financial activities. Being a social geek, I love to keep up and discuss latest technologies & digital trends. 
-
-In my free time, I enjoy playing the guitar, board/video gaming as well as hiking, kayaking & exploring the outdoors.
-
-I'm fascinated about personality assessments & quizzes. Here's a list of my personality profiles:
-Ideation-Strategic-Restorative-Communication-Command (StrengthsFinder2.0) 
-ENTP (MBTI) 
-Loyal-Skeptic (Enneagram) 
-Mid-O High-C High-E Mid-A Low-N (Big5)
-Fire (TetraMap)
-</AboutDesc>
-</About>
-<Contact offset={3}>
-<Inner>
-<Title>Contact</Title>
-<ContactText>
-            Say <a href="mailto:victor.tqw@live.com">Hi</a> or follow me on <a href="https://www.instagram.com/victorxmusic/">Instagram</a> & <a href="https://www.linkedin.com/in/victortqw/">LinkedIn</a> 
-</ContactText>
-</Inner>
-</Contact>
-</Parallax>
-</>
-)
-
-export default Index
+  <>
+    <Layout />
+    <Parallax pages={5}>
+      <Hero offset={0}>
+        <BigTitle>
+          Hello, <br /> I'm Victor.
+        </BigTitle>
+        <Subtitle>Welcome to my online portfolio!</Subtitle>
+      </Hero>
+      <Projects offset={1}>
+        <Title>Projects</Title>
+        <ProjectsWrapper>
+          <ProjectCard
+            title="Digital Marketing Executive | Funding Societies"
+            link="https://fundingsocieties.com/"
+            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+          >
+            Funding Societies | Modalku is the largest Peer-to-Peer lending
+            platform in Southeast Asia. It provides business financing to SMEs
+            which is crowdfunded by retail and institutional investors.
+          </ProjectCard>
+          <ProjectCard
+            title="Marketing Lead | CareerSocius"
+            link="https://www.careersocius.com/"
+          >
+            CareerSocius is a social enterprise that helps job-seekers in
+            Singapore maximize their career potential. With a diverse team of
+            Branding Buddies spanning across various industries, we offer the
+            relevant expertise and experience to guide job seekers in their job
+            search.
+          </ProjectCard>
+        </ProjectsWrapper>
+      </Projects>
+      <About offset={3}>
+        <Title>About</Title>
+        <AboutHero>
+          <Avatar src={avatar} alt="Victor Tan" />
+          <AboutSub>
+            Victor Tan | Digital Marketer & Fintech Enthusiast
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          Adaptable, motivated and self-driven Economics & Finance major with
+          cross-disciplinary skills sets involving marketing, analytics,
+          finance, design and strategy. I am passionate about the FinTech
+          industry and the use of technology to improve and automate financial
+          activities. Being a social geek, I love to keep up and discuss latest
+          technologies & digital trends. In my free time, I enjoy playing the
+          guitar, board/video gaming as well as hiking, kayaking & exploring the
+          outdoors. I'm fascinated about personality assessments & quizzes.
+          Here's a list of my personality profiles:
+          Ideation-Strategic-Restorative-Communication-Command
+          (StrengthsFinder2.0) ENTP (MBTI) Loyal-Skeptic (Enneagram) Mid-O
+          High-C High-E Mid-A Low-N (Big5) Fire (TetraMap)
+        </AboutDesc>
+      </About>
+      <Contact offset={4}>
+        <Inner>
+          <Title>Contact</Title>
+          <ContactText>
+            Say <a href="mailto:victor.tqw@live.com">Hi</a> or follow me on{" "}
+            <a href="https://www.instagram.com/victorxmusic/">Instagram</a> &{" "}
+            <a href="https://www.linkedin.com/in/victortqw/">LinkedIn</a>
+          </ContactText>
+        </Inner>
+        <Footer>&copy; 2019 by Victor </Footer>
+      </Contact>
+    </Parallax>
+  </>
+);
+export default Index;
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -26,22 +26,6 @@ const InnerWave = styled.div`
 
 const Contact = ({ children, offset }) => (
   <>
-    <Divider fill="#23262b" speed={0.2} offset={offset}>
-      <WaveWrapper>
-        <InnerWave>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-            <path>
-              <animate
-                attributeName="d"
-                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                repeatCount="indefinite"
-                dur="30s"
-              />
-            </path>
-          </svg>
-        </InnerWave>
-      </WaveWrapper>
-    </Divider>
     <Content speed={0.4} offset={offset}>
       {children}
     </Content>
